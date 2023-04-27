d790b33dc240dea152e24cd1a36c4cc6cd1b694e
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2023-04-27 07:23:04 +0530

    Update phonenumber-scraper.py
    Added comments

--- phonenumber-scraper.py
+++ phonenumber-scraper.py
@@ -6,20 +6,26 @@ from tkinter import messagebox as msg
 import phonenumbers as pn
 from phonenumbers import*
 from phonenumbers.carrier import name_for_number
-top= Tk()
-top.title("Phone number scraper")
-top.geometry("500x200")
-k = Label(text="Enter the phone number with country code",font="40")
-e = Entry(bd=5, font="36")
+
+#Main function where, the phone number is sent to library for carrier analysis
 def main():
 	try:
 		device = str(name_for_number(a.parse(e.get(),None),"en"))
 		msg.showinfo("Carrier",device)
 	except:
 		msg.showerror("Carrier","Try Again with - +<countrycode><yournumber>\n eg.= +919388*****")
+
+#GUI of app
+top= Tk()
+top.title("Phone number scraper")
+top.geometry("500x200")
+k = Label(text="Enter the phone number with country code",font="40")
+e = Entry(bd=5, font="36")
 bb = Button(text="Scrap",command=main,relief="groove",activebackground="black", activeforeground="white",background="white",font=36)
 
+#Positioning of widgets in Tkinter window
 k.place(x=10,y=20)
 e.place(x=60,y=50)
 bb.place(x=110,y=100)
+
 top.mainloop()
